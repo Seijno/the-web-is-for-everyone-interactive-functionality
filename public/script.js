ea537ec706b81e3a50c0ccf2baaef9e2b114c1e8
Seijno <seijnovannieuwkoop@gmail.com>
2024-05-07 11:43:29 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
+// variables
 const appear = document.querySelector(".quantityChoice");
 appear.style.display = 'block';
 
+const reveal = document.getElementById("popupField");
+
+// evenlisteners
+reveal.addEventListener('click', revealPopup)
+
+
+// functies
 function checked(){
     document.getElementById('selected').innerHTML = getCheckboxCount();
   }
   
-  function getCheckboxCount() {
+function getCheckboxCount() {
     return document.querySelectorAll('input[type=checkbox]:checked').length;
-  }
+}
   
-  document.querySelectorAll("input").forEach(i=>{
-   i.onclick = () => checked();
-  });
+document.querySelectorAll("input").forEach(input=>{
+    input.onclick = () => checked();
+});
+
+function revealPopup(){
+    reveal;
+    element.classList.toggle("popupFieldReveal");
+}
--- server.js
+++ server.js
@@ -2,27 +2,26 @@ import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
 const app = express()
+const apiUrl = 'https://fdnd-agency.directus.app/items'
+const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
+
+let sdgChosen = []
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-
-let sdgChosen = []
-
-const apiUrl = 'https://fdnd-agency.directus.app/items'
-const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
 // console.log(sdgData.data)
 
 app.get('/', (request, response) =>  {
-    fetchJson(apiUrl + '/hf_sdgs').then((sdgData) =>{
-	response.render('index', {
-        sdgs: sdgData.data
-
+  fetchJson(apiUrl + '/hf_sdgs').then((sdgData) =>{
+	  response.render('index', {
+    sdgs: sdgData.data
     })
+  })
 })
-})
+
 app.post('/', (request, response) =>{
   console.log(request.body)
   response.redirect(303, '/')
